459b312e8c7ed1eee970ae344adfbc99256536cb
anmol verma <anmol.verma4@gmail.com>
2022-10-14 11:47:24 +0530

    udpate: build.sh

--- build.sh
+++ build.sh
@@ -24,9 +24,12 @@ cd gRPC-KMP
 ./gradlew grpc-multiplatform-lib:build && ./gradlew grpc-multiplatform-lib:publishToMavenLocal
 ./gradlew plugin:build && ./gradlew plugin:publishToMavenLocal
 
+cd ..
+
 echo "<----Cloning slack_multiplatform_protos---->"
 git clone https://github.com/oianmol/slack_multiplatform_protos
 
+cd ..
 echo "<----Cloning slack_multiplatform_generate_protos---->"
 git clone https://github.com/oianmol/slack_multiplatform_generate_protos
 cd slack_multiplatform_generate_protos
@@ -34,12 +37,14 @@ git submodule update --init --recursive
 echo "<----Building slack_multiplatform_protos---->"
 ./gradlew build && ./gradlew publishToMavenLocal
 
+cd ..
 echo "<----Building slack_multiplatform_client_data_lib---->"
-cd .. && git clone https://github.com/oianmol/slack_multiplatform_client_data_lib && cd slack_multiplatform_client_data_lib
+git clone https://github.com/oianmol/slack_multiplatform_client_data_lib && cd slack_multiplatform_client_data_lib
 ./gradlew build && ./gradlew publishToMavenLocal
 
+cd ..
 echo "<----Running slack_multiplatform_grpc_server---->"
-cd .. && git clone https://github.com/oianmol/slack_multiplatform_grpc_server && cd slack_multiplatform_grpc_server
+git clone https://github.com/oianmol/slack_multiplatform_grpc_server && cd slack_multiplatform_grpc_server
 ./gradlew run
 
 #echo "<----Running SlackComposeMultiplatform Client---->"
