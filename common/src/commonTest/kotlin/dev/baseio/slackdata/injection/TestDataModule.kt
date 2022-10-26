496586fc1bc3ea49767825c6e67713a43234e302
Anmol Verma <anmol.verma4@gmail.com>
2022-10-26 13:35:40 +0530

    add: android test cases!

--- common/src/commonTest/kotlin/dev/baseio/slackdata/injection/TestDataModule.kt
+++ common/src/commonTest/kotlin/dev/baseio/slackdata/injection/TestDataModule.kt
@@ -3,10 +3,7 @@ package dev.baseio.slackdata.injection
 import dev.baseio.database.SlackDB
 import dev.baseio.grpc.GrpcCalls
 import dev.baseio.grpc.IGrpcCalls
-import dev.baseio.slackdata.DriverFactory
 import dev.baseio.slackdata.localdata.testDbConnection
-import dev.baseio.slackdata.network.FakeGrpcCalls
-import org.koin.core.module.Module
 import org.koin.dsl.module
 
 val testDataModule = module {
