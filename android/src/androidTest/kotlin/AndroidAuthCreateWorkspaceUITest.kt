42f6ec4221d35f79ef23cbf4fc04f7134d45c217
Anmol Verma <anmol.verma4@gmail.com>
2022-10-27 08:58:57 +0530

    fix: ui test

--- android/src/androidTest/kotlin/AndroidAuthCreateWorkspaceUITest.kt
+++ android/src/androidTest/kotlin/AndroidAuthCreateWorkspaceUITest.kt
@@ -1,7 +1,6 @@
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.performClick
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.arkivanov.decompose.DefaultComponentContext
@@ -9,18 +8,27 @@ import com.arkivanov.essenty.lifecycle.LifecycleRegistry
 import dev.baseio.android.MobileApp
 import dev.baseio.database.SlackDB
 import dev.baseio.slackclone.RootComponent
-import dev.baseio.slackclone.initKoin
+import dev.baseio.slackclone.data.injection.viewModelDelegateModule
 import dev.baseio.slackdata.DriverFactory
 import dev.baseio.slackdata.SKKeyValueData
+import dev.baseio.slackdata.injection.dataMappersModule
+import dev.baseio.slackdata.injection.fakeDataSourceModule
+import dev.baseio.slackdata.injection.testDispatcherModule
+import dev.baseio.slackdata.injection.testUseCaseModule
+import dev.baseio.slackdomain.CoroutineDispatcherProvider
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.core.KoinApplication
+import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
+import org.koin.dsl.module
 
 @RunWith(AndroidJUnit4::class)
 class AndroidAuthCreateWorkspaceUITest {
@@ -29,29 +37,53 @@ class AndroidAuthCreateWorkspaceUITest {
 
     private val lifecycle = LifecycleRegistry()
     private val skKeyValueData = SKKeyValueData(ApplicationProvider.getApplicationContext())
-    private val rootComponent by lazy { RootComponent(DefaultComponentContext(lifecycle = lifecycle)) }
+    private val rootComponent by lazy { RootComponent(DefaultComponentContext(lifecycle = lifecycle)).apply {
+        navigateCreateWorkspace(true)
+    } }
     lateinit var koinApplication: KoinApplication
 
     @Before
     fun prepare() {
-        koinApplication = initKoin({ skKeyValueData },
-            { DriverFactory(ApplicationProvider.getApplicationContext()).createDriver(SlackDB.Schema) })
+        koinApplication = startKoin {
+            modules(
+                module {
+                    single {
+                        skKeyValueData
+                    }
+                    single {
+                        SlackDB.invoke(DriverFactory(ApplicationProvider.getApplicationContext()).createDriver(SlackDB.Schema))
+                    }
+                },
+                testUseCaseModule,
+                viewModelDelegateModule,
+                dataMappersModule,
+                fakeDataSourceModule,
+                testDispatcherModule
+            )
+        }
+        Dispatchers.setMain(koinApplication.koin.get<CoroutineDispatcherProvider>().main)
     }
 
     @Test
-    fun createWorkspaceWhenCredentialsAreValid() {
-        compose.apply {
-            setContent {
-                MobileApp({
-                    rootComponent
-                }, koinApplication)
+    fun whenLoginToSlackClickedItNavigatesToTheLoginScreen() {
+        runBlocking(koinApplication.koin.get<CoroutineDispatcherProvider>().main) {
+            with(compose) {
+                mainClock.autoAdvance = false
+                setContent {
+                    MobileApp({
+                        rootComponent
+                    }, koinApplication)
+                }
+                awaitIdle()
+                mainClock.advanceTimeBy(5000)
+                onNodeWithText("Login to Slack").assertIsDisplayed()
             }
-            onNodeWithText("Sign In to Slack").performClick()
         }
     }
 
     @After
     fun teardown() {
+        Dispatchers.resetMain()
         stopKoin()
     }
 }
