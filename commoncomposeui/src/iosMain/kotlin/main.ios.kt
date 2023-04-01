e300152c621db32be118c7fb5fcbfc9fb62a74f2
Anmol Verma <anmol.verma4@gmail.com>
2023-04-01 20:10:07 +0200

    update: remove useless imports

--- commoncomposeui/src/iosMain/kotlin/main.ios.kt
+++ commoncomposeui/src/iosMain/kotlin/main.ios.kt
@@ -1,28 +1,17 @@
-import androidx.compose.foundation.background // ktlint-disable import-ordering
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Application
 import androidx.compose.ui.window.ComposeUIViewController
 import com.arkivanov.decompose.DefaultComponentContext
 import com.arkivanov.essenty.lifecycle.LifecycleRegistry
-import dev.baseio.slackclone.SlackApp
-import dev.baseio.slackclone.RootComponent
-import dev.baseio.slackclone.WindowInfo
-import dev.baseio.slackclone.LocalWindow
-import dev.baseio.slackclone.commonui.theme.LocalSlackCloneColor
+import dev.baseio.slackclone.*
 import dev.baseio.slackclone.commonui.theme.SlackCloneTheme
-import dev.baseio.slackclone.initKoin
 import kotlinx.cinterop.useContents
 import platform.UIKit.UIApplication
-import platform.UIKit.UIWindow
 import platform.UIKit.UIViewController
+import platform.UIKit.UIWindow
 
 val lifecycle = LifecycleRegistry()
 val rootComponent by lazy {
